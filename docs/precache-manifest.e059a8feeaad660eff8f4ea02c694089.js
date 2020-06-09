d82830b36c0b7ee24b49ca1e1f6bc309314fc873
Sébastien Piquemal <sebpiq@gmail.com>
2020-06-09 18:17:53 +0200

    deploy

---
+++ docs/precache-manifest.e059a8feeaad660eff8f4ea02c694089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c49e69848c22d81b117deaecb3aeb794",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1e61e2275da3bd584e5",
+    "url": "/static/css/main.1040394a.chunk.css"
+  },
+  {
+    "revision": "f47ab203e23e4bba64a1",
+    "url": "/static/js/2.17d7d3cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.17d7d3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1e61e2275da3bd584e5",
+    "url": "/static/js/main.80eb80cc.chunk.js"
+  },
+  {
+    "revision": "8cfbf6021e38f9a8243f",
+    "url": "/static/js/runtime-main.dd91100c.js"
+  }
+]);
