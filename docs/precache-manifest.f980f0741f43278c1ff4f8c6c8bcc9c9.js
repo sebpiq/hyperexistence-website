72755d1d3770c0a850c918d21af8ce760ff5b0ff
Sébastien Piquemal <sebpiq@gmail.com>
2022-01-08 15:43:24 +0100

    deploy

---
+++ docs/precache-manifest.f980f0741f43278c1ff4f8c6c8bcc9c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc334a8fe537ee30357cec31718b2810",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b87b13836e9dc6f1b2",
+    "url": "/static/js/2.09ee69ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.09ee69ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bab3c173dde627434c4",
+    "url": "/static/js/main.1481970b.chunk.js"
+  },
+  {
+    "revision": "8cfbf6021e38f9a8243f",
+    "url": "/static/js/runtime-main.dd91100c.js"
+  }
+]);
